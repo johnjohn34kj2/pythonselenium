93731561950a226be90e634fd66b9884649d6635
johnjohn34kj2 <121136757+johnjohn34kj2@users.noreply.github.com>
2023-01-14 06:47:37 +0200

    Update Selenium-Template.py

--- Selenium-Template.py
+++ Selenium-Template.py
@@ -38,26 +38,83 @@ for option in options:
     chrome_options.add_argument(option)
 
     
-driver = webdriver.Chrome(options = chrome_options)
-driver.set_window_size(2000, 2000)
-wait = WebDriverWait(driver, 8)
-driver.get("https://direct-link.net/552433/fetch-rewards")
-time.sleep(5)
-driver.save_screenshot('screenie.png')
+
 try:
+  driver = webdriver.Chrome(options=options)
+  driver.set_window_size(2000, 2000)
+  wait = WebDriverWait(driver, 8)
+  driver.get("https://link-hub.net/581468/fetch-rewards")
+  time.sleep(5)
+  driver.save_screenshot('screenie.png')
+  try:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
+  except:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
+  driver.save_screenshot('screenie.png')
+  try:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="top"]/div[1]/div[1]/mat-icon'))).click()
+  except:
+    print ("error")
+  driver.save_screenshot('screenie.png')
   wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
-except:
-  wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]'))).click()
-  wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
-driver.save_screenshot('screenie.png')
-try:
-  wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="top"]/div[1]/div[1]/mat-icon'))).click()
+  wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-download-redirect-wrapper-component/lv-redirect/div[1]/div[1]/div/div/lv-redirect-first-page/lv-download-page/div/div/div/div[2]/div/lv-button/a'))).click()
+  driver.save_screenshot('screenie.png')
+  time.sleep(3)
+  driver.quit()
 except:
   print ("error")
-driver.save_screenshot('screenie.png')
-wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
-wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-download-redirect-wrapper-component/lv-redirect/div[1]/div[1]/div/div/lv-redirect-first-page/lv-download-page/div/div/div/div[2]/div/lv-button/a'))).click()
-driver.save_screenshot('screenie.png')
-time.sleep(3)
 
+try:
+  driver = webdriver.Chrome(options=options)
+  driver.set_window_size(2000, 2000)
+  wait = WebDriverWait(driver, 8)
+  driver.get("https://direct-link.net/581468/how-to-install-any-pc")
+  time.sleep(5)
+  driver.save_screenshot('screenie.png')
+  try:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
+  except:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
+  driver.save_screenshot('screenie.png')
+  try:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="top"]/div[1]/div[1]/mat-icon'))).click()
+  except:
+    print ("error")
+  driver.save_screenshot('screenie.png')
+  wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
+  wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-download-redirect-wrapper-component/lv-redirect/div[1]/div[1]/div/div/lv-redirect-first-page/lv-download-page/div/div/div/div[2]/div/lv-button/a'))).click()
+  driver.save_screenshot('screenie.png')
+  time.sleep(3)
+  driver.quit()
+except:
+  print ("error")
+
+
+try:
+  driver = webdriver.Chrome(options=options)
+  driver.set_window_size(2000, 2000)
+  wait = WebDriverWait(driver, 8)
+  driver.get("https://direct-link.net/581468/earn-money-for-free")
+  time.sleep(5)
+  driver.save_screenshot('screenie.png')
+  try:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
+  except:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]'))).click()
+    wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
+  driver.save_screenshot('screenie.png')
+  try:
+    wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="top"]/div[1]/div[1]/mat-icon'))).click()
+  except:
+    print ("error")
+  driver.save_screenshot('screenie.png')
+  wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-redirect/div[1]/div[1]/div/div[1]/lv-redirect-first-page/lv-countdown-block/div/mat-card[2]/div[2]/div/div[3]/div/div/lv-button/a'))).click()
+  wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/lv-root/div[1]/mat-sidenav-container/mat-sidenav-content/div[2]/lv-download-redirect-wrapper-component/lv-redirect/div[1]/div[1]/div/div/lv-redirect-first-page/lv-download-page/div/div/div/div[2]/div/lv-button/a'))).click()
+  driver.save_screenshot('screenie.png')
+  time.sleep(3)
+  driver.quit()
+except:
+  print ("error") 
 
